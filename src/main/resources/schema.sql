57c1d9833bad1da3f92c3b582bc28c93a5142cc5
Henry <henry.arcaya@sptech.school>
2025-10-15 21:03:12 -0300

    chore: configuração das tabelas basicas

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,6 +9,36 @@ CREATE TABLE logs (
     createdAt TIMESTAMP(6)
 );
 
+CREATE TABLE Rodovia (
+    idRodovia INT NOT NULL PRIMARY KEY auto_increment,
+    nomeRodovia VARCHAR(45),
+    denominacaoRodovia VARCHAR(45),
+    nomeConcessionaria VARCHAR(45),
+    municipioRodovia VARCHAR(45),
+    regionalDer VARCHAR(45),
+    regAdmMunicipio VARCHAR(45)
+);
+
+
+-- Tabela Acidente
+CREATE TABLE Acidente (
+    idAcidente INT NOT NULL,
+    marcoKm DECIMAL(10, 2), -- Ajuste o tamanho e precisão conforme necessário
+    dataAcidente DATE,
+    horaAcidente DATETIME, -- Armazenando data e hora do acidente
+    tipoAcidente VARCHAR(45),
+    causaAcidente VARCHAR(45),
+    clima VARCHAR(45),
+    veiculosEnvolvidos VARCHAR(45),
+    vitFatal INT,
+    vitGrave INT,
+    vitLeve INT,
+    tipoPista VARCHAR(45),
+    Rodovia_idRodovia INT NOT NULL, -- Chave estrangeira para Rodovia
+    PRIMARY KEY (idAcidente),
+    FOREIGN KEY (Rodovia_idRodovia) REFERENCES Rodovia(idRodovia)
+);
+
 select * from logs;
 
 INSERT INTO logs (typeLog, description, createdAt)

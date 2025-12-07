b7b74cafe9754ee7fe3e5201320a0ba599d84213
Henry <henry.arcaya@sptech.school>
2025-12-06 22:01:13 -0300

    refatorando etl

--- src/main/java/br/com/moveon/services/ETLService.java
+++ src/main/java/br/com/moveon/services/ETLService.java
@@ -33,11 +33,8 @@ public class ETLService extends AbstractService {
     public void execute() {
         logger.info("Executando ETL Da Artesp");
         try {
-            HashMap<String, Integer> mapConcessionariaFk = this.processarConcessionarias();
-            HashMap<Rodovia, Integer> mapRodoviaFk = this.processarRodovias(mapConcessionariaFk);
-
             logger.info("Iniciando processo de processar todos acidentes da base de dados");
-            this.processarAcidentes(mapConcessionariaFk, mapRodoviaFk);
+            this.processarBaseDeDados();
 
             logger.info("Enviando alerta para canal #alerts no slack");
             slackService.sendMessage("#moveon-alerts", SlackDefaultMessages.SUCCESS_PROCESS);
@@ -49,115 +46,74 @@ public class ETLService extends AbstractService {
         }
     }
 
-    public HashMap<String, Integer> processarConcessionarias() {
-        ConcessionariaDao concessionariaDao = this.entityDaos.stream()
-                .filter(dao -> dao instanceof ConcessionariaDao)
-                .map( cDao -> (ConcessionariaDao) cDao).findFirst().get();
+    public void processarBaseDeDados() {
+        ConcessionariaDao concessionariaDao = this.entityDaos.stream().filter(dao -> dao instanceof ConcessionariaDao).map(dao -> (ConcessionariaDao) dao).findFirst().get();
+        RodoviaDao rodoviaDao = this.entityDaos.stream().filter(dao -> dao instanceof RodoviaDao).map(dao -> (RodoviaDao) dao).findFirst().get();
+        AcidenteDao acidenteDao = this.entityDaos.stream().filter(dao -> dao instanceof AcidenteDao).map(dao -> (AcidenteDao) dao).findFirst().get();
 
         concessionariaDao.truncate();
 
         HashMap<String, Integer> mapConcessionariaFk = new HashMap<>();
-        List<Concessionaria> buffer = new ArrayList<>();
-
-        try {
-            for (String fileName : fileNames) {
-                try (Workbook workbook = WorkbookFactory.create(new FileInputStream(fileName))) {
-                    Iterator<Row> iterator = workbook.getSheetAt(0).rowIterator();
-                    if (iterator.hasNext()) iterator.next(); // pula header
-
-                    while (iterator.hasNext()) {
-                        Row row = iterator.next();
-                        String nome = row.getCell(ExcelColumnIndex.CONCESSIONARIA_NOME).toString();
-                        if (!mapConcessionariaFk.containsKey(nome)) {
-                            Concessionaria concessionaria = new Concessionaria(mapConcessionariaFk.size() + 1, row);
-                            mapConcessionariaFk.put(concessionaria.getNome(), concessionaria.getIdConcessionaria());
-                            buffer.add(concessionaria);
-                        }
-                    }
-                }
-            }
-            concessionariaDao.saveAll(buffer);
-            logger.info("Concessionarias cadastradas: " + buffer.size());
-        } catch (Exception e) {
-            logger.error("Erro ao salvar concessionarias: " + e.getMessage());
-            System.exit(0);
-        }
-        return mapConcessionariaFk;
-    }
-
-    public HashMap<Rodovia, Integer> processarRodovias(HashMap<String, Integer> mapConcessionariaFk) {
-        RodoviaDao rodoviaDao = this.entityDaos.stream()
-                .filter(dao -> dao instanceof RodoviaDao)
-                .map( rDao -> (RodoviaDao) rDao).findFirst().get();
         HashMap<Rodovia, Integer> mapRodoviaFk = new HashMap<>();
-        List<Rodovia> buffer = new ArrayList<>();
 
-        try {
-            for (String fileName : fileNames) {
-                try (Workbook workbook = WorkbookFactory.create(new FileInputStream(fileName))) {
-                    Iterator<Row> iterator = workbook.getSheetAt(0).rowIterator();
-                    if (iterator.hasNext()) iterator.next();
+        List<Concessionaria> bufferConcessionarias = new ArrayList<>();
+        List<Rodovia> bufferRodovias = new ArrayList<>();
+        List<Acidente> bufferAcidentes = new ArrayList<>();
 
-                    while (iterator.hasNext()) {
-                        Row row = iterator.next();
-                        if (Rodovia.validaParaSalvar(row)) {
-                            Integer fkConcessionaria = mapConcessionariaFk.get(row.getCell(ExcelColumnIndex.CONCESSIONARIA_NOME).toString());
-                            Rodovia rodovia = new Rodovia(row, fkConcessionaria);
-                            if (!mapRodoviaFk.containsKey(rodovia)) {
-                                Integer idRodovia = mapRodoviaFk.size() + 1;
-                                rodovia.setIdRodovia(idRodovia);
-                                mapRodoviaFk.put(rodovia, idRodovia);
-                                buffer.add(rodovia);
-                            }
-                        }
-                    }
-                }
-            }
-            rodoviaDao.saveAll(buffer);
-            logger.info("Rodovias cadastradas: " + buffer.size());
-        } catch (Exception e) {
-            logger.error("Erro ao salvar rodovias: " + e.getMessage());
-            System.exit(0);
-        }
-        return mapRodoviaFk;
-    }
-
-    public void processarAcidentes(HashMap<String, Integer> mapConcessionariaFk, HashMap<Rodovia, Integer> mapRodoviaFk) {
-        AcidenteDao acidenteDao = this.entityDaos.stream()
-                .filter(dao -> dao instanceof AcidenteDao)
-                .map( aDao -> (AcidenteDao) aDao).findFirst().get();
         try {
             for (String fileName : fileNames) {
                 logger.info("Lendo o Arquivo: " + fileName);
 
                 try (Workbook workbook = WorkbookFactory.create(new FileInputStream(fileName))) {
                     Iterator<Row> iterator = workbook.getSheetAt(0).rowIterator();
-                    if (iterator.hasNext()) iterator.next();
+                    if (iterator.hasNext()) iterator.next(); // pula header
 
-                    List<Acidente> buffer = new ArrayList<>();
                     while (iterator.hasNext()) {
                         Row row = iterator.next();
-                        Rodovia rodovia = new Rodovia(row, mapConcessionariaFk.get(row.getCell(ExcelColumnIndex.CONCESSIONARIA_NOME).toString()));
-                        rodovia.setIdRodovia(mapRodoviaFk.get(rodovia));
 
-                        if (Acidente.validoParaSalvar(row)) {
-                            Acidente acidente = new Acidente(row, rodovia);
-                            buffer.add(acidente);
+                        // Concessionária
+                        String nomeConcessionaria = row.getCell(ExcelColumnIndex.CONCESSIONARIA_NOME).toString();
+                        if (!mapConcessionariaFk.containsKey(nomeConcessionaria)) {
+                            Concessionaria concessionaria = new Concessionaria(mapConcessionariaFk.size() + 1, row);
+                            mapConcessionariaFk.put(concessionaria.getNome(), concessionaria.getIdConcessionaria());
+                            bufferConcessionarias.add(concessionaria);
+                        }
 
-                            if (row.getRowNum() % 10000 == 0) {
-                                logger.info("Lendo da linha " + (row.getRowNum() - 10000) + " Até " + row.getRowNum());
-                            } else if (row.getRowNum() == workbook.getSheetAt(0).getLastRowNum()) {
-                                logger.info("Lendo da linha " + ((row.getRowNum() / 10000) * 10000) + " Até " + row.getRowNum());
+                        // Rodovia
+                        if (Rodovia.validaParaSalvar(row)) {
+                            Integer fkConcessionaria = mapConcessionariaFk.get(nomeConcessionaria);
+                            Rodovia rodovia = new Rodovia(row, fkConcessionaria);
+                            if (!mapRodoviaFk.containsKey(rodovia)) {
+                                Integer idRodovia = mapRodoviaFk.size() + 1;
+                                rodovia.setIdRodovia(idRodovia);
+                                mapRodoviaFk.put(rodovia, idRodovia);
+                                bufferRodovias.add(rodovia);
                             }
                         }
+
+                        // Acidente
+                        if (Acidente.validoParaSalvar(row)) {
+                            Rodovia rodovia = new Rodovia(row, mapConcessionariaFk.get(nomeConcessionaria));
+                            rodovia.setIdRodovia(mapRodoviaFk.get(rodovia));
+                            Acidente acidente = new Acidente(row, rodovia);
+                            bufferAcidentes.add(acidente);
+                        }
                     }
-                    acidenteDao.saveAll(buffer);
-                    buffer.clear();
                 }
             }
-            logger.info("Acidentes processados e salvos com sucesso.");
+
+            logger.info("Iniciando processamento em lote da captura dos dados");
+            // Salvar em lote
+            concessionariaDao.saveAll(bufferConcessionarias);
+            logger.info("Concessionarias Salvas: " + bufferConcessionarias.size());
+
+            rodoviaDao.saveAll(bufferRodovias);
+            logger.info("Rodovias Salvas: " + bufferRodovias.size());
+
+            acidenteDao.saveAll(bufferAcidentes);
+            logger.info("Acidentes Salvas: " + bufferAcidentes.size());
         } catch (Exception e) {
-            logger.error("Erro ao salvar acidentes: " + e.getMessage());
+            logger.error("Erro ao processar ETL: " + e.getMessage());
             System.exit(0);
         }
     }
